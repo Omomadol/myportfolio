96814fd672706865e6d80b0630486ca302b0f530
Omomadol <bennettomomadol@gmail.com>
2024-10-22 15:25:48 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,48 @@
-const hamMenu = document.querySelector('.ham-menu');
-const offScreenMenu = document.querySelector('.off-screen-menu');
+// const hamMenu = document.querySelector('.ham-menu');
+// const offScreenMenu = document.querySelector('.off-screen-menu');
 
-// Toggle the menu when the hamburger icon is clicked
-hamMenu.addEventListener('click', () => {
-    hamMenu.classList.toggle('active');
-    offScreenMenu.classList.toggle('active');
-});
+// // Toggle the menu when the hamburger icon is clicked
+// hamMenu.addEventListener('click', () => {
+//     hamMenu.classList.toggle('active');
+//     offScreenMenu.classList.toggle('active');
+// });
+
+const slides = document.querySelectorAll(".porimg");
+let slideIndex = 0;
+let intervaliid = null;
+
+// initializeSlider();
+document.addEventListener("DOMContentLoaded", initializeSlider);
+
+function initializeSlider(){
+
+    if(slides.length > 0) {
+        slides[slideIndex].classList.add("displaySlide");
+        intervaliid = setInterval(nextSlide, 5000);
+    }
+
+}
+function showSlide(index){
+
+    if(index  >= slides.length){
+            slideIndex = 0
+    }
+    else if(index < 0){
+        slideIndex = slides.length - 1;
+    }
+    slides.forEach(slide => {
+        slide.classList.remove("displaySlide");
+    });
+    slides[slideIndex].classList.add("displaySlide");
+}
+function prevSlide(){
+    clearInterval(intervaliid);
+    slideIndex--;
+    showSlide(slideIndex)
+}
+function nextSlide(){
+    slideIndex++;
+    showSlide(slideIndex);
+}
+
+
